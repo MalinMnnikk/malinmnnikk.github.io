281c5c8ff63240ad5952cbc756024d2021bd52e5
Malin Männikkö <malin.mannikko@gmail.com>
2019-01-11 07:54:28 +0100

    Color edit

--- styles.css.pp
+++ styles.css.pp
@@ -1,9 +1,10 @@
 #lang pollen
-◊(define h1-color "white")
+◊(define h1-color "#f3eef2")
 ◊(define strong-color "pink")
-◊(define h2-color "gray")
+◊(define h2-color "#B288AB")
 
 body {padding: 2em; font-size: 20px;}
-h1 {background: ◊|h1-color|; color: gray;}
-h2 {color: gray;}
+h1 {background: ◊|h1-color|; color: #65024f;
+padding: 1rem; font-family: helvetica;}
+h2 {color: #B288AB;}
 strong {color: ◊|strong-color|;}
